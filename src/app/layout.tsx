37c9e3bb4aed8edf0d68373e9428d5edf46b9cfb
muhammadAbdulMannan2022 <muhammadabdulmannan21@gmail.com>
2026-08-17 09:14:31 +0600

    style: add display: swap property to Inter and Outfit font configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter", display: "swap" });
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit", display: "swap" });
 
 export const metadata: Metadata = {
   title: "Workvence | Complete Solution for Freelancing",

1b7a3f35c0cd43eac4e3fdc245810b4378f70276
naimekattor <naimekattor@gmail.com>
2026-09-10 12:52:10 +0600

    comment standalone form next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone",
   images: {
     qualities: [75, 85],
     remotePatterns: [
